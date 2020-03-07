2fda17aa8dc94bd6bd3812f243e70b1fe70011a0
AdrianAlbrecht <155376@student.uwm.edu.pl>
2020-03-07 14:48:13 +0100

    sobota 07-03-20

--- 1_3_3/main.c
+++ 1_3_3/main.c
@@ -3,6 +3,17 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int a,b,c;
+    scanf("%i %i %i", &a, &b, &c);
+    if(a>=b)
+        if(a>=c)
+            printf("%i",a);
+        else
+            printf("%i",c);
+    else
+        if(b>=c)
+            printf("%i",b);
+        else
+            printf("%i",c);
     return 0;
 }
